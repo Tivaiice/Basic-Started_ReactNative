9db5f3339b8ce0e155afbf3742b668af1fbec596
Tiva Wongratkatanyoo <tivaice@Tivas-MacBook-Pro.local>
2020-05-07 14:17:11 +0700

    commit 4

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -4,23 +4,27 @@ import { Text, StyleSheet, View, FlatList } from 'react-native'
 
 const ListScreen = () => {
     const friends = [
-        {name : "Friend #1", key : '1'},
-        {name : "Friend #2", key : '2'},
-        {name : "Friend #3", key : '3'},
-        {name : "Friend #4", key : '4'},
-        {name : "Friend #5", key : '5'},
-        {name : "Friend #6", key : '6'},
-        {name : "Friend #7", key : '7'},
-        {name : "Friend #8", key : '8'},
-        {name : "Friend #9", key : '9'},
-        {name : "Friend #10", key : '10'},
+        {name : "Friend #1", age: 20},
+        {name : "Friend #2", age: 21},
+        {name : "Friend #3", age: 22},
+        {name : "Friend #4", age: 23},
+        {name : "Friend #5", age: 24},
+        {name : "Friend #6", age: 25},
+        {name : "Friend #7", age: 26},
+        {name : "Friend #8", age: 27},
+        {name : "Friend #9", age: 28},
+        {name : "Friend #10", age: 29},
     ];
 
     return (
-        <View>
-            <Text> ListScreen </Text>
-            <FlatList data={friends} renderItem={({item}) => {
-                return <Text>{item.name}</Text>
+        <View>  
+            
+            <FlatList 
+                // horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                keyExtractor={friend => friend.name}
+                data={friends} renderItem={({item}) => {
+                return <Text style={{marginHorizontal:50, marginVertical: 50 }}>{item.name} - Age {item.age}</Text>
             }}/>
         </View>
     )
